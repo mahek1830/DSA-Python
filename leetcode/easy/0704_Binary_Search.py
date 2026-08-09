05aaaf46858327b63edf72e6ec978678e53bc405
mahek1830 <mahekakeel12@gmail.com>
2026-08-09 21:47:31 +0530

    0704_Binary_Search.py

---
+++ leetcode/easy/0704_Binary_Search.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left=0
+        right=(len(nums)-1)
+        mid=(left+right)//2
+
+        while left<=right:
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                left=mid+1
+                mid=(left+right)//2
+            elif nums[mid]>target:
+                right=mid-1
+                mid=(left+right)//2
+        return -1
